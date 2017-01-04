4b3a27de95ed58d81733162dcb061a2e79335717
simdax <simoncornaz@gmail.com>
2017-01-04 19:04:39 +0100

    tchiop

--- game/board.js
+++ game/board.js
@@ -12,23 +12,49 @@ var moveIndex;
 var marker;
 var hexagonGroup;
 
-// bon c'est un peu nul, mais bon...
-var un = [[0,0],[-1,-1],[-1,0],[0,-1],[0,1],[1,1],[1,0]]
-// var deux = [[-2,-2],[-2,-1],[-2,-2],[1,-2],[1,1],[1,2],[2,-2],[2,1]]
-// var trois = [[-1,-1],[-1,0],[-1,-1],[0,-1],[0,0],[0,1],[1,-1],[1,0]]
+var select=true;
+var hl=true;
 
-// deux= [];
-// for(var i = -2; i < 3; i++) {
-//     for(var j = -2; j < 3; j++) {
-//         deux.push([i,j])
-//     }
-// };
-// for(var i = 1; i < 4; i++) {
-//     for(var j = 1; j < 4; j++) {
-//         var ii=i+j;
-//         deux[ii]=undefined
-//     }
-// };
+// bon l'algo d'entourag est un peu nul, mais bon...
+var un = [[0,0],[-1,-1],[-1,0],[0,-1],[0,1],[1,1],[1,0]]
+
+function highlight () {
+        var pos=moveIndex;
+        hexagonGroup.setAll('alpha', 0.1);
+        var indices=[];
+        for(var i = 0; i < un.length; i++) {
+            if (un[i]!==undefined) {
+                var x= un[i][0]+pos.x;
+                var y= un[i][1]+pos.y;
+                // nawak...
+                if (pos.x%2==1) {
+                    if(un[i][0]==(-1)){y+=1 }
+                }else{
+                    if(un[i][0]==(1)){y-=1 }
+                };
+                if (x>=0 && y>=0 && x<gridSizeX && y<gridSizeY) {
+                    indices.push ( convert(x,y) )
+                }
+            }
+        };
+        for(var i = 0; i < indices.length; i++) {
+            hexagonGroup.getAt(indices[i]).alpha=1;
+        }
+};
+function normal() {
+    hexagonGroup.setAll('alpha', 1)
+}
+
+var mouse;
+Mouse=function () {   
+}
+Mouse.prototype={
+    go:function () {
+        select = !select
+    },
+
+};
+mouse=new Mouse();
 
 //maths
 function convert (x,y) {
@@ -36,10 +62,10 @@ function convert (x,y) {
 };
 
 
+
 marker=function () {
+    this.mvts=3;
     this.sprite;
-    this.pos;
-    this.landed=true;
 };
 marker.prototype={
     preload:function (game) {
@@ -50,41 +76,14 @@ marker.prototype={
     create:function (game) {
         this.sprite = game.add.sprite(0,0,"marker");
 	this.sprite.anchor.setTo(0.5);
-        this.sprite.scale.setTo(0.5);
+        this.sprite.scale.setTo(0.85);
 	this.sprite.visible=false;
-    },
-
-    
-    move:function () {
-//        if (this.pos===undefined) {
-            this.pos=moveIndex
-  //      };
-        if (this.landed) {
-            hexagonGroup.setAll('alpha', 1);
-            this.landed=false
-        }else{
-            hexagonGroup.setAll('alpha', 0.1);
-            var indices=[];
-            for(var i = 0; i < un.length; i++) {
-                if (un[i]!==undefined) {
-                    var x= un[i][0]+this.pos.x;
-                    var y= un[i][1]+this.pos.y;
-                    // nawak...
-                    if (this.pos.x%2==1) {
-                        if(un[i][0]==(-1)){y+=1 }
-                    }else{
-                        if(un[i][0]==(1)){y-=1 }
-                    };
-                    if (x>=0 && y>=0 && x<gridSizeX && y<gridSizeY) {
-                        indices.push ( convert(x,y) )
-                    }
-                }
-            };
-            for(var i = 0; i < indices.length; i++) {
-                hexagonGroup.getAt(indices[i]).alpha=1;
-            }
-            this.landed=true
-        };
+        this.sprite.inputEnabled=true;
+        this.sprite.events.onInputDown.add(function () {
+            if (hl) {
+                normal(); hl=false;
+            }else{highlight(); hl=true}
+        }, this);
     },
 };
 
@@ -96,6 +95,7 @@ board= function (game) {
 board.prototype={  
     render:function () {
         game.debug.text("appuyez sur x pour switcher", 100,100)
+        game.debug.text(select, 100,150)
     },
     preload:function () {
 	game.load.image("hexagonM", "images/hexagon.png");
@@ -103,40 +103,17 @@ board.prototype={
         this.joueur1=new marker; this.joueur1.preload(game)
     },
     create: function() {
-	hexagonGroup = game.add.group();
-	game.stage.backgroundColor = "#000000"
-	for(var i = 0; i < gridSizeX/2; i ++){
-	    for(var j = 0; j < gridSizeY; j ++){
-		if(gridSizeX%2==0 || i+1<gridSizeX/2 || j%2==0){
-		    var hexagonX = hexagonWidth*i*1.5+(hexagonWidth/4*3)*(j%2);
-		    var hexagonY = hexagonHeight*j/2;	
-		    var hexagon;
-                    var x = (Math.floor(Math.random() * 2) == 0);
-                    if(x){
-                        hexagon= game.add.sprite(hexagonX,hexagonY,"hexagonT");
-                    }else{
-                        hexagon= game.add.sprite(hexagonX,hexagonY,"hexagonM");
-                    };
-                    // scalage bourrin
-                    //hexagon.scale.setTo(0.138,0.185);
-		    hexagonGroup.add(hexagon);
-		}
-	    }
-	}
-	hexagonGroup.y = (game.height-hexagonHeight*Math.ceil(gridSizeY/2))/2;
-        if(gridSizeY%2==0){
-            hexagonGroup.y-=hexagonHeight/4;
-        }
-	hexagonGroup.x = (game.width-Math.ceil(gridSizeX/2)*hexagonWidth-Math.floor(gridSizeX/2)*hexagonWidth/2)/2;
-        if(gridSizeX%2==0){
-            hexagonGroup.x-=hexagonWidth/8;
-        }
+        createHexGrp();
         this.joueur1.create(game);
+        
 	hexagonGroup.add(this.joueur1.sprite);
-
+        this.placeMarker(0,0);
+        
         // events
         game.input.addMoveCallback(this.checkHex, this);
-        game.input.onDown.add(this.joueur1.move, this)
+        game.input.onDown.add(mouse.go,this);
+
+        
         // changing state
         var key=game.input.keyboard.addKey(Phaser.Keyboard.X);
         key.onDown.add(this.goTo, this)
@@ -148,44 +125,41 @@ board.prototype={
         game.state.start("interieur")
     },
     checkHex: function(){
-        if (this.joueur1.landed) {            
-            var candidateX = Math.floor((game.input.worldX-hexagonGroup.x)/sectorWidth);
-            var candidateY = Math.floor((game.input.worldY-hexagonGroup.y)/sectorHeight);
-            var deltaX = (game.input.worldX-hexagonGroup.x)%sectorWidth;
-            var deltaY = (game.input.worldY-hexagonGroup.y)%sectorHeight; 
-            if(candidateX%2==0){
-                if(deltaX<((hexagonWidth/4)-deltaY*gradient)){
+        var candidateX = Math.floor((game.input.worldX-hexagonGroup.x)/sectorWidth);
+        var candidateY = Math.floor((game.input.worldY-hexagonGroup.y)/sectorHeight);
+        var deltaX = (game.input.worldX-hexagonGroup.x)%sectorWidth;
+        var deltaY = (game.input.worldY-hexagonGroup.y)%sectorHeight; 
+        if(candidateX%2==0){
+            if(deltaX<((hexagonWidth/4)-deltaY*gradient)){
+                candidateX--;
+                candidateY--;
+            }
+            if(deltaX<((-hexagonWidth/4)+deltaY*gradient)){
+                candidateX--;
+            }
+        }    
+        else{
+            if(deltaY>=hexagonHeight/2){
+                if(deltaX<(hexagonWidth/2-deltaY*gradient)){
                     candidateX--;
-                    candidateY--;
-                }
-                if(deltaX<((-hexagonWidth/4)+deltaY*gradient)){
-                    candidateX--;
-                }
-            }    
-            else{
-                if(deltaY>=hexagonHeight/2){
-                    if(deltaX<(hexagonWidth/2-deltaY*gradient)){
-                        candidateX--;
-                    }
-                }
-                else{
-                    if(deltaX<deltaY*gradient){
-                        candidateX--;
-                    }
-                    else{
-                        candidateY--;
-                    }
                 }
             }
-            moveIndex={x:candidateX, y:candidateY};
-            if(hexagonGroup.getAt(convert(candidateX,candidateY)).alpha==1)
-                {this.placeMarker(candidateX,candidateY)};
-        }else{
-            this.colorizeHex()
+            else{
+                if(deltaX<deltaY*gradient){
+                    candidateX--;
+                }
+                else{
+                    candidateY--;
+                }
+            }
         }
-    },
-    colorizeHex:function (posX,posY) {
-        hexagonGroup.getRandom.tint=Math.random() * 0xffffff;
+        moveIndex={x:candidateX, y:candidateY};
+        if (select) {
+            if(hexagonGroup.getAt(convert(candidateX,candidateY)).alpha==1)
+            {
+                this.placeMarker(candidateX,candidateY);             
+            }
+        };
     },
     placeMarker: function(posX,posY){
 	if(posX<0 || posY<0 || posX>=gridSizeX || posY>columns[posX%2]-1){
@@ -204,4 +178,35 @@ board.prototype={
 	}
     }
     
+}
+
+function createHexGrp(arg) {
+    hexagonGroup = game.add.group();
+    game.stage.backgroundColor = "#000000"
+    for(var i = 0; i < gridSizeX/2; i ++){
+	for(var j = 0; j < gridSizeY; j ++){
+	    if(gridSizeX%2==0 || i+1<gridSizeX/2 || j%2==0){
+		var hexagonX = hexagonWidth*i*1.5+(hexagonWidth/4*3)*(j%2);
+		var hexagonY = hexagonHeight*j/2;	
+		var hexagon;
+                var x = (Math.floor(Math.random() * 2) == 0);
+                if(x){
+                    hexagon= game.add.sprite(hexagonX,hexagonY,"hexagonT");
+                }else{
+                    hexagon= game.add.sprite(hexagonX,hexagonY,"hexagonM");
+                };
+                // scalage bourrin
+                //hexagon.scale.setTo(0.138,0.185);
+		hexagonGroup.add(hexagon);
+	    }
+	}
+    }
+    hexagonGroup.y = (game.height-hexagonHeight*Math.ceil(gridSizeY/2))/2;
+    if(gridSizeY%2==0){
+        hexagonGroup.y-=hexagonHeight/4;
+    }
+    hexagonGroup.x = (game.width-Math.ceil(gridSizeX/2)*hexagonWidth-Math.floor(gridSizeX/2)*hexagonWidth/2)/2;
+    if(gridSizeX%2==0){
+        hexagonGroup.x-=hexagonWidth/8;
+    }
 }
--- game/interieur.js
+++ game/interieur.js
@@ -1,12 +1,129 @@
 
+// //trios en haut
+// game.add.text(100,60,"2");
+// game.add.text(230,60,"2");
+// game.add.text(350,60,"2");
+
+// // trois en bas
+// game.add.text(100,470,"2");
+// game.add.text(230,470,"2");
+// game.add.text(400,420,"2");
+
+// // trois au milieu
+// game.add.text(100,270,"2");
+// game.add.text(230,300,"2");
+// game.add.text(350,200,"2");
+
+var bebete=function (){ };
+bebete.prototype={
+    move:function () {
+        
+    },
+    attaque:function (bebete) {
+        bebete.meurt()
+    },
+    meurt:function () {
+          this.sprite.destroy()
+    },
+}
+
+var Espece={
+    nb:2,
+}
+
+var Especes= {
+    mollusques: {
+        annelides:{proies:[],xp:0},
+        escargots:{proies:[],xp:0}
+    },
+    cephalopodes:{
+        seches:{proies:["annelides"],xp:3},
+        pieuvres:{proies:["thons"],xp:3}
+    },
+    arthropodes:{
+        insectes:{proies:[],xp:1},
+        arachnides:{proies:["insectes"],xp:4}
+    },
+    selaciens:{
+        roussettes:{proies:["seches"],xp:3},
+        requins:{proies:["coelacanthes"],xp:3}
+    },
+    osteoichtyens:{
+        thons:{proies:["annelides"],xp:3},
+        coelacanthes:{proies:["thons"],xp:3}
+    },
+    anapasides:{
+        grenouilles:{proies:["insectes"],xp:3},
+        serpents:{proies:["rongeurs"],xp:3},
+        crocodiles:{proies:["serpents","compsognathus"],xp:4}
+    },
+    dinosauriens:{
+        compsognathus:{proies:["grenouilles"],xp:2},
+        tyrannosaures:{proies:["hippopotames","compsognathus","gorilles"],xp:7}
+    },
+    cetaces:{
+        orques:{proies:["pieuvres","requins"],xp:7}
+    },
+    mammiferes:{
+        rongeur:{proies:["escargots"],xp:3},
+        hippopotame:{proies:["crocodile"],xp:3},
+        gorille:{proies:[],xp:2}
+    }
+};
+
+
+var esp=function (mere,proies,xp) {
+    this.mere=mere;
+    this.proies=proies;
+};
+esp.prototype=Object.create(Phaser.Sprite.prototype, {
+    meurt:function () {
+        this.destroy();
+        mere.meurt()
+    }
+});
+
+var phylum=function (name,available) {
+    this.name=name; this.nb=2;
+    this.available=available;
+    this.sprites=[];
+    for(var i = 0; i < 2; i++) {
+        sprites.add(new esp(this))
+    }
+    
+};
+phylum.prototype={
+    meurt:function () {
+        this.nb-=1;
+        //game.remove.sprite
+    }
+};
+
 interieur = function (game) {};
 interieur.prototype={
     preload: function () {
-        game.load.image("interieur","interieurIncubateur.jpg" )
+        game.load.image("interieur","images/interieurIncubateur.jpg" )
     },
     create:function () {
         var img=game.add.image(0,0, "interieur")
-        img.scale.setTo(0.5);
+        img.scale.setTo(1,0.85);
+        game.add.text(30,0,"20");
+
+        //trios en haut
+        game.add.text(100,60,"2");
+        game.add.text(230,60,"2");
+        game.add.text(350,60,"2");
+
+        // trois en bas
+        game.add.text(100,470,"2");
+        game.add.text(230,470,"2");
+        game.add.text(400,420,"2");
+
+        // trois au milieu
+        game.add.text(100,270,"2");
+        game.add.text(230,300,"2");
+        game.add.text(350,200,"2");
+        
         var key=game.input.keyboard.addKey(Phaser.Keyboard.X);
         key.onDown.add(this.goTo, this)
     },
