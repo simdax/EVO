ae2b342da36e8d8c3b5b63455062bdaac5a2f299
simdax <simoncornaz@gmail.com>
2017-02-04 19:23:16 +0100

    added html test

---
+++ public/mpgame/game/assets/images/intérieur du vaisseau/test.js
@@ -0,0 +1,7 @@
+var game = new Phaser.Game(1000,600,Phaser.AUTO,'phaser',{preload:preload,create:create});
+
+function preload() {
+}
+function create() {
+    game.stage.backgroundColor=0x000000;
+}
